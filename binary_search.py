8b32e812f6f8b3cbe6cec3587f9f13ffae787342
Chebichii-Lab <chebichiiserem@gmail.com>
2024-01-08 20:48:43 +0300

    define the binary search function

--- binary_search.py
+++ binary_search.py
@@ -15,3 +15,30 @@ def naive_search(l, target):
 
 # binary search used divide and conquer!
 # we will leverage the fact that our list is SORTED
+def binary_search(l, target, low=None, high=None):
+    if low is None:
+        low = 0
+    if high is None:
+        high = len(l) -1
+
+    if high < low:
+        return -1
+    
+    # example of l = [1, 3, 5, 10, 12]
+    # searching for 10
+    # should return 3 because 10 is at index 3
+    midpoint = (low + high) // 2  # 2
+
+    if l(midpoint) == target:
+        return midpoint
+    elif target < l [midpoint]:
+        return binary_search(l, target, low, midpoint-1)
+    else:
+        # target > l[midpoint]
+        return binary_search(l, target, midpoint+1, high)
+    
+if __name__ == '__main__':
+    l = [1, 3, 5, 10, 12]
+    target = 10
+    print(naive_search(l, target))
+    print(binary_search(l, target))
