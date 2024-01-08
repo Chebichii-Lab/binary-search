902d5674a8ad6848565b0952fdcd8e482fa1548d
Chebichii-Lab <chebichiiserem@gmail.com>
2024-01-08 20:28:03 +0300

    define a function on naive search

--- binary_search.py
+++ binary_search.py
@@ -5,3 +5,13 @@
 # naive search: scan entire list and ask if its equal to the target
 # if yes, return the index
 # if no, then return -1
+
+def naive_search(l, target):
+    # example l = [1, 3, 10, 12]
+    for i in range(len(l)):
+        if l[i] == target:
+            return i
+    return -1
+
+# binary search used divide and conquer!
+# we will leverage the fact that our list is SORTED
