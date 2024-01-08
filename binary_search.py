8c600fd7eb6fc4eccef6b5fa6681fb7444c7763f
Chebichii-Lab <chebichiiserem@gmail.com>
2024-01-08 20:23:18 +0300

    add comment description of the project

---
+++ binary_search.py
@@ -0,0 +1,7 @@
+# implememtation of binary search algorithm
+
+# we will prove that binary search os faster than naive search
+
+# naive search: scan entire list and ask if its equal to the target
+# if yes, return the index
+# if no, then return -1
